441b1cbeedd46cc8200c1999f858493cee78f9c0
Your Name <you@example.com>
2019-06-26 16:13:00 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,10 @@
+const number1 = 5;
+const number2 = 8;
+const sum =number1 + number2
+const division =number2 / number1
+const a = "barev"
+const b ="hajox"
+const naxadasutyun = a+" kam "+b
+console.log(naxadasutyun)
+console.log(2)
+
